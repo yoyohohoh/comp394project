f3b3cb68b5cf5aca2e956dd58656293eb4a9922a
Yiu Yiu Yoyo Ho <yho45@my.centennialcollege.ca>
2024-11-02 23:21:06 -0400

    menu, level ui

---
+++ GrimReaper_LossOfMemories/Assets/_Scripts/MenuController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenMenu()
+    {
+        Debug.Log("open main menu");
+        menu.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        Debug.Log("close main menu");
+        menu.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
